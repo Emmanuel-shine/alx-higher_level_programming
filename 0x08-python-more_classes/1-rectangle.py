84be95a8193d7994f92bcf260589c6ac63c1a6fb
Emmanuel-shine <emmanuelshine9@gmail.com>
2024-04-25 05:09:38 -0700

    more classes hh clear

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -1,23 +1,3 @@
-class Rectangle:
-    """
-    Represents a rectangle.
-
-    Attributes:
-        __width (int): The width of the rectangle.
-        __height (int): The height of the rectangle.
-    """
-
-    def __init__(self, width=0, height=0):
-        """
-        Initializes a new Rectangle instance.
-
-        Args:
-            width (int): The width of the rectangle (default 0).
-            height (int): The height of the rectangle (default 0).
-        """
-        self.width = width
-        self.height = height
-
     @property
     def width(self):
         """
@@ -75,4 +55,3 @@ class Rectangle:
             raise ValueError("height must be >= 0")
         else:
             self.__height = value
-```
